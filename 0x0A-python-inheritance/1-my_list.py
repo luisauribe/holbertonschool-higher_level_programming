15609ac1ebc52c49c9d591b9e1451acb5c6a9f83
luisauribe <luribeasprilla@gmail.com>
2021-03-02 02:55:13 -0500

    class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module for MyList class."""
-
-
-class MyList(list):
-    """This class inherits from list."""
-    def print_sorted(self):
-        """This method print the sorted list."""
-        print(sorted(self))
